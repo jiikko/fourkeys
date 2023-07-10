19d13b519251eb3b1dad7b8e0acab88925b4fafc
jiikko <n905i.1214@gmail.com>
2023-07-10 16:26:11 +0900

    Update variables for terraform

--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -1,8 +1,12 @@
 module "fourkeys" {
-  source              = "../modules/fourkeys"
-  project_id          = var.project_id
-  enable_apis         = var.enable_apis
-  region              = var.region
-  bigquery_region     = var.bigquery_region
-  parsers             = var.parsers
+  source          = "../modules/fourkeys"
+  project_id      = var.project_id
+  enable_apis     = var.enable_apis
+  region          = var.region
+  bigquery_region = var.bigquery_region
+  parsers         = var.parsers
+}
+
+provider "google" {
+  project = "ubiregi-dev-productivity-vis"
 }
